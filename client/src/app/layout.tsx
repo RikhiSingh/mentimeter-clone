d9cd4e70cc910b8b39d057b456c94ffab3e01ef1
rikhisingh <rikhisinghnov@gmail.com>
2024-06-08 16:14:01 -0400

    footer added to home page

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "@/components/Providers";
+import { Footer } from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>{children}</Providers>
+        <Providers>{children}</Providers>        
+      <Footer />     
       </body>
     </html>
   );
---
+++ client/src/components/footer.tsx
@@ -0,0 +1,48 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
+export const Footer = () => {
+    return (
+        <footer className="lg:block h-20 w-full border-t-2 border-slate-200 p-2 -mt-[80px]">
+            <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+                <Link href="https://github.com/RikhiSingh" target="_blank" passHref>
+                    <Button size="lg" variant="ghost" className="w-full">
+                        <Image
+                            src="/github.svg"
+                            alt="Github"
+                            height={40}
+                            width={40}
+                            className="mr-4 rounded -md"
+                        />
+                        RikhiSingh
+                    </Button>
+                </Link>
+                <Link href="https://www.linkedin.com/in/rikhi-singh/" target="_blank" passHref>
+                    <Button size="lg" variant="ghost" className="w-full">
+                        <Image
+                            src="/linkedIn.png"
+                            alt="LinkedIn"
+                            height={43}
+                            width={43}
+                            className="mr-4 rounded -md"
+                        />       
+                        rikhi-singh                 
+                    </Button>
+                </Link>
+                <Link href="https://www.instagram.com/rikhi_singh/" target="_blank" passHref>
+                    <Button size="lg" variant="ghost" className="w-full">
+                        <Image
+                            src="/igLogo.svg"
+                            alt="Instagram"
+                            height={40}
+                            width={40}
+                            className="mr-4 rounded -md"
+                        />    
+                        rikhi_singh                    
+                    </Button>
+                </Link>
+            </div>
+        </footer>
+    );
+};
